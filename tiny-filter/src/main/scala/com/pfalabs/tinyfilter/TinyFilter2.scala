2b8ebeb484d25372fe41825577607ef02bd0a6bb
Alex Parvulescu <alex@pfalabs.com>
2015-11-23 22:01:13 +0100

    tweaked properties

--- tiny-filter/src/main/scala/com/pfalabs/tinyfilter/TinyFilter2.scala
+++ tiny-filter/src/main/scala/com/pfalabs/tinyfilter/TinyFilter2.scala
@@ -10,11 +10,9 @@ import org.apache.felix.scr.annotations.Reference
 @Component(immediate = true, specVersion = "1.2")
 @Service(value = Array(classOf[Filter]))
 @Properties(Array(
-  new Property(name = "osgi.http.whiteboard.context.name", value = Array("org.osgi.service.http")),
-//  new Property(name = "osgi.http.whiteboard.filter.dispatcher", value = Array("REQUEST")),
-  new Property(name = "osgi.http.whiteboard.filter.pattern", value = Array("/tf2")),
-  new Property(name = "osgi.http.whiteboard.filter.name", value = Array("TinyFilter2"))
-))
+  new Property(name = "osgi.http.whiteboard.context.select", value = Array("(osgi.http.whiteboard.context.name=org.osgi.service.http)")),
+  new Property(name = "osgi.http.whiteboard.filter.pattern", value = Array("/t")),
+  new Property(name = "osgi.http.whiteboard.filter.name", value = Array("TinyFilter2"))))
 class TinyFilter2 extends Filter {
 
   val log: Logger = LoggerFactory.getLogger(classOf[TinyFilter2]);
@@ -29,9 +27,14 @@ class TinyFilter2 extends Filter {
   override def destroy() = log.info("Destroyed filter");
 
   override def doFilter(req: ServletRequest, res: ServletResponse, chain: FilterChain) {
-    res.setContentType("text/plain");
-    val out = res.getWriter();
-    out.println("Request = " + req);
-    out.println("Service = " + tiny.doSomething);
+    log.info("request! {}", req);
+
+    //    res.setContentType("text/plain");
+    //    val out = res.getWriter();
+    //    out.println("Request = " + req);
+    //    out.println("Service = " + tiny);
+
+    chain.doFilter(req, res)
+
   }
 }
